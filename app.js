c89f0276701cadab50889fb024c1a13556769d5c
Christian <chnikel@gmail.com>
2021-10-17 14:33:21 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Static Files
+app.use(express.static('public'));
+app.use('/css', express.static(__dirname + 'public/css'));
+app.use('/img', express.static(__dirname + 'public/img'));
+app.use('/js', express.static(__dirname + 'public/js'));
+
+app.get('/', (req, res) => {
+	res.send('Ok');
+});
+
+// Listen on port 5000
+app.listen(port, () => console.log(`Listening on port ${port}`));
